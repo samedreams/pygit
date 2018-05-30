4ce1d3d3ee3d0692e7075b0a260377e4007684d1
wujunze <itwujunze@163.com>
2018-05-30 15:53:01 +0800

    first commit

---
+++ pygit.py
@@ -0,0 +1,24 @@
+import os
+
+
+def read_file(path):
+    """Read contents of file at given path as bytes."""
+    with open(path, 'rb') as f:
+        return f.read()
+
+
+def write_file(path, data):
+    """Write data bytes to file at given path."""
+    with open(path, 'wb') as f:
+        f.write(data)
+
+
+def init(repo):
+    """创建仓库,初始化.git目录"""
+    os.mkdir(repo)
+    os.mkdir(os.path.join(repo, '.git'))
+    for name in ['objects', 'refs', 'refs/heads']:
+        os.mkdir(os.path.join(repo, '.git', name))
+    write_file(os.path.join(repo, '.git', 'HEAD'),
+               b'ref: refs/heads/master')
+    print('initialized empty repository: {}'.format(repo))
